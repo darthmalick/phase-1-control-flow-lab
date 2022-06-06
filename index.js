d0381cb334be37df3bd62795dcb3822925d1a426
mark harrison <darthmalick@gmail.com>
2022-06-06 14:43:33 -0500

    commit tree 1a7547aaa039411f3e1c3f4340dbd4f1c985fce5

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let response
+ if (distance <= 400){
+ response = "This one is on me!";
+return response}
+ else if (distance > 400 && distance <= 2000){
+ response = "That will be twenty bucks.";
+return response}
+else if (distance > 2000 && distance <= 2500){
+  response = "I will gladly take your thirty bucks.";
+  return response
+}
+else if (distance > 2500){
+  response = "No can do.";
+  return response
+}
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+const citymessage = (city === 'NYC') ? ("Ok, sounds good.") : ("No go.")
+return citymessage
+ 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+switch (tip){
+case 'generous':
+  return 'Thank you so much.'
+case 'not as generous':
+  return 'Thank you.'
+  default:
+    return 'Bye.'}
 }
